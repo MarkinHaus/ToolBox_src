2128dc127b159401ea2dab935ce928029b6b48a3
Markin <markinhausmanns@gmail.com>
2022-11-22 21:42:17 +0100

    API V2.3 - Fist

--- api.py
+++ api.py
@@ -15,6 +15,8 @@ class PostRequest(BaseModel):
 app = FastAPI()
 
 origins = [
+    "http://194.233.168.22",
+    "http://194.233.168.22:8000",
     "http://127.0.0.1:8080",
     "http://localhost:8080",
     "https://simpelm.com",
